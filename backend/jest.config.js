4f40780b5d17ce2a6fb396234b87f96b590953f9
malik LAFIA <maliksero.lafia@gmail.com>
2025-01-27 10:10:12 +0100

    initialisation du projet avec les dépendance de bases (jest, express, mongoose, etc)

---
+++ backend/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    testEnvironment: 'node',
+    roots: ['<rootDir>/src', '<rootDir>/tests'],
+    testMatch: ['**/__tests__/**/*.js', '**/?(*.)+(spec|test).js'],
+    coverageDirectory: 'coverage',
+    collectCoverageFrom: [
+        'src/**/*.js',
+        '!src/infrastructure/**/*.js'
+    ]
+};
