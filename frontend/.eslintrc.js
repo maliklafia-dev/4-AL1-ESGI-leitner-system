a6de26709aff7d2f2e198c61700ba33a3eb9e4fa
celina <chabcelina@gmail.com>
2025-01-28 22:47:03 +0100

    Init Front & config Eslint

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "next/core-web-vitals",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: [
+    "react",
+    "@typescript-eslint",
+    "prettier",
+  ],
+  rules: {
+    "prettier/prettier": "error", // Active Prettier comme linter
+    "react/react-in-jsx-scope": "off", // Désactivé car Next.js n'a pas besoin d'importer React
+  },
+};
