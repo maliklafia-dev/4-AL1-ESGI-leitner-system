f331447fa120ebfd5f53173adeb3a9a5a881b96e
Celina Chabane <celina.chabane@acensi.fr>
2025-02-14 15:16:43 +0100

    Configuration linter & add it to Makefile

--- backend/src/infrastructure/database/seeders/seedUser.js
+++ backend/src/infrastructure/database/seeders/seedUser.js
@@ -1,36 +1,37 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import bcrypt from 'bcrypt'; 
-import UserModel from '../models/UserModel.js';
-
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import bcrypt from "bcrypt";
+import UserModel from "../models/UserModel.js";
 
 dotenv.config();
 
 const seedUser = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
-    console.log('MongoDB connected for seeding...');
+    console.log("MongoDB connected for seeding...");
 
-    const existingUser = await UserModel.findOne({ email: 'rrrrr@example.com' });
+    const existingUser = await UserModel.findOne({
+      email: "rrrrr@example.com",
+    });
     if (existingUser) {
-      console.log('User already exists. Skipping seeding.');
+      console.log("User already exists. Skipping seeding.");
       process.exit();
     }
 
-    const hashedPassword = await bcrypt.hash('rrrrr', 10);
+    const hashedPassword = await bcrypt.hash("rrrrr", 10);
 
     const user = new UserModel({
-      email: 'rrrr@example.com',
-      username: 'rrr',
-      password: hashedPassword
+      email: "rrrr@example.com",
+      username: "rrr",
+      password: hashedPassword,
     });
 
     await user.save();
-    console.log('User seeded successfully:', user);
+    console.log("User seeded successfully:", user);
 
-    process.exit(); 
+    process.exit();
   } catch (error) {
-    console.error('Error seeding user:', error);
+    console.error("Error seeding user:", error);
     process.exit(1);
   }
 };
