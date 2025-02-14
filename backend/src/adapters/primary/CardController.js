bd600180f18ae6b703f0566239dcf5c9d0ecdfda
Celina Chabane <celina.chabane@acensi.fr>
2025-02-15 00:07:45 +0100

    version stable

--- backend/src/adapters/primary/CardController.js
+++ backend/src/adapters/primary/CardController.js
@@ -1,57 +1,57 @@
-import express from 'express';
+import express from "express";
 
 class CardController {
-    constructor(cardService) {
-        this.router = express.Router();
-        this.cardService = cardService;
+  constructor(cardService) {
+    this.router = express.Router();
+    this.cardService = cardService;
 
-        this.router.get('/cards', this.getAllCards.bind(this));
-        this.router.post('/cards', this.createCard.bind(this));
-        this.router.get('/cards/quizz', this.getQuizCards.bind(this));
-        this.router.patch('/cards/:cardId/answer', this.answerCard.bind(this));
-    }
+    this.router.get("/cards", this.getAllCards.bind(this));
+    this.router.post("/cards", this.createCard.bind(this));
+    this.router.get("/cards/quizz", this.getQuizCards.bind(this));
+    this.router.patch("/cards/:cardId/answer", this.answerCard.bind(this));
+  }
 
-    getAllCards(req, res) {
-        try {
-            const tags = req.query.tags ? req.query.tags.split(',') : [];
-            const cards = this.cardService.getAllCards(tags);
-            res.status(200).json(cards);
-        } catch (error) {
-            res.status(400).json({ error: error.message });
-        }
+  getAllCards(req, res) {
+    try {
+      const tags = req.query.tags ? req.query.tags.split(",") : [];
+      const cards = this.cardService.getAllCards(tags);
+      res.status(200).json(cards);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
     }
+  }
 
-    createCard(req, res) {
-        try {
-            const newCard = this.cardService.createCard(req.body);
-            res.status(201).json(newCard);
-        } catch (error) {
-            res.status(400).json({ error: error.message });
-        }
+  createCard(req, res) {
+    try {
+      const newCard = this.cardService.createCard(req.body);
+      res.status(201).json(newCard);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
     }
+  }
 
-    getQuizCards(req, res) {
-        try {
-            const date = req.query.date ? new Date(req.query.date) : new Date();
-            const cards = this.cardService.getCardsForQuiz(date);
-            res.status(200).json(cards);
-        } catch (error) {
-            res.status(400).json({ error: error.message });
-        }
+  getQuizCards(req, res) {
+    try {
+      const date = req.query.date ? new Date(req.query.date) : new Date();
+      const cards = this.cardService.getCardsForQuiz(date);
+      res.status(200).json(cards);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
     }
+  }
 
-    answerCard(req, res) {
-        try {
-            const { isValid } = req.body;
-            if (isValid === undefined) {
-                return res.status(400).json({ error: 'isValid is required' });
-            }
-            this.cardService.answerCard(req.params.cardId, isValid);
-            res.status(204).send();
-        } catch (error) {
-            res.status(400).json({ error: error.message });
-        }
+  answerCard(req, res) {
+    try {
+      const { isValid } = req.body;
+      if (isValid === undefined) {
+        return res.status(400).json({ error: "isValid is required" });
+      }
+      this.cardService.answerCard(req.params.cardId, isValid);
+      res.status(204).send();
+    } catch (error) {
+      res.status(400).json({ error: error.message });
     }
+  }
 }
 
-module.exports = new CardController().router;
+export default CardController;
--- backend/src/infrastructure/server.js
+++ backend/src/infrastructure/server.js
@@ -1,37 +1,91 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-import CardController from '../adapters/primary/CardController.js';
-import CardService from '../domain/services/CardService.js';
-import CardRepository from '../adapters/secondary/repositories/CardRepository.js';
-import swaggerJsdoc from 'swagger-jsdoc';
-import swaggerUi from 'swagger-ui-express';
-import QuizRepository from '../adapters/secondary/repositories/QuizRepository.js';
-import QuizService from '../domain/services/QuizService.js'; 
+// import express from 'express';
+// import dotenv from 'dotenv';
+// import mongoose from 'mongoose';
+// import CardController from '../adapters/primary/CardController.js';
+// import CardService from '../domain/services/CardService.js';
+// import CardRepository from '../adapters/secondary/repositories/CardRepository.js';
+// import swaggerJsdoc from 'swagger-jsdoc';
+// import swaggerUi from 'swagger-ui-express';
+// import QuizRepository from '../adapters/secondary/repositories/QuizRepository.js';
+// import QuizService from '../domain/services/QuizService.js'; 
 
+// dotenv.config();
+
+// console.log("Starting server...");
+// const app = express();
+
+// app.use(express.json());
+
+// const PORT = process.env.PORT || 8080;
+ 
+// const swaggerOptions = {
+//     definition: {
+//         openapi: '3.0.0',
+//         info: {
+//             title: 'Card API',
+//             version: '1.0.0',
+//             description: 'API for managing flashcards',
+//         },
+//     },
+//     apis: ['./src/adapters/primary/CardController.js'],
+// };
+
+// const swaggerSpec = swaggerJsdoc(swaggerOptions);
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+// const connectDB = async () => {
+//     try {
+//         if (!process.env.MONGODB_URI) {
+//             throw new Error('MongoDB URI is not defined in environment variables');
+//         }
+//         await mongoose.connect(process.env.MONGODB_URI);
+//         console.log("MongoDB Connected");
+
+//         const cardRepository = new CardRepository(mongoose.connection);
+//         const cardService = new CardService(cardRepository);
+//         const cardController = new CardController(cardService);
+//         app.use('/cards', cardController.getRouter());
+
+//         const quizRepository = new QuizRepository(mongoose.connection);
+//         const quizService = new QuizService(quizRepository);
+       
+//         } catch (error) {
+//         console.error("MongoDB Connection Error:", error);
+//         process.exit(1);
+//     }
+// };
+
+// await connectDB();
+
+// app.get("/", (req, res) => {
+//     console.log("Route accessed");
+//     res.send("Connected to MongoDB - Server is running successfully!");
+// });
+
+// app.use((err, req, res, next) => {
+//     console.error(err.stack);
+//     res.status(500).json({ 
+//         message: 'Something went wrong!',
+//         error: err.message 
+//     });
+// });
+
+// app.listen(PORT, () => {
+//     console.log(`Server ==> http://localhost:${PORT}`);
+// });
+
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+// Load env vars before anything else
 dotenv.config();
 
 console.log("Starting server...");
+
 const app = express();
-
-app.use(express.json());
-
 const PORT = process.env.PORT || 8080;
- 
-const swaggerOptions = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'Card API',
-            version: '1.0.0',
-            description: 'API for managing flashcards',
-        },
-    },
-    apis: ['./src/adapters/primary/CardController.js'],
-};
 
-const swaggerSpec = swaggerJsdoc(swaggerOptions);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+// Database connection
 const connectDB = async () => {
     try {
         if (!process.env.MONGODB_URI) {
@@ -39,21 +93,13 @@ const connectDB = async () => {
         }
         await mongoose.connect(process.env.MONGODB_URI);
         console.log("MongoDB Connected");
-
-        const cardRepository = new CardRepository(mongoose.connection);
-        const cardService = new CardService(cardRepository);
-        const cardController = new CardController(cardService);
-        app.use('/cards', cardController.getRouter());
-
-        const quizRepository = new QuizRepository(mongoose.connection);
-        const quizService = new QuizService(quizRepository);
-       
-        } catch (error) {
+    } catch (error) {
         console.error("MongoDB Connection Error:", error);
         process.exit(1);
     }
 };
 
+// Connect to database
 await connectDB();
 
 app.get("/", (req, res) => {
@@ -61,14 +107,6 @@ app.get("/", (req, res) => {
     res.send("Connected to MongoDB - Server is running successfully!");
 });
 
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ 
-        message: 'Something went wrong!',
-        error: err.message 
-    });
-});
-
 app.listen(PORT, () => {
-    console.log(`Server ==> http://localhost:${PORT}`);
+    console.log(`Server ==> http://localhost:${PORT}`);  
 });
